6ab8f5d283ec73ef7a6df8839131c71a7ca9825e
Abel Rodriguez <69878476+arcodez@users.noreply.github.com>
2021-05-10 15:36:06 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import dynamic from "next/dynamic";
+import Head from "next/head";
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import("../components/Paypal"),
+  { ssr: false }
+);
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <script src="https://www.paypal.com/sdk/js?client-id=AWKytFGu4kNLPRMxebIkzgS7cZKW5rxfZ6AwwLldnmQR5p1vIGWMB5RJs76VESbVp3rduTNESnQzmOrg"></script>
+      </Head>
+      <DynamicComponentWithNoSSR />
+    </div>
+  );
+}
+
+export default Home;
